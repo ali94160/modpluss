667d98dfa8e323c425a3bc9eb4064c71ca172a6f
Ali <72066097+ali94160@users.noreply.github.com>
2025-01-28 03:17:56 +0100

    watchlist-HF

--- server/controllers/watchlistController.js
+++ server/controllers/watchlistController.js
@@ -2,7 +2,7 @@ import { Watchlist } from "../models/Watchlist.js";
 
 export const updateWatchlist = async (req, res) => {
   try {
-    const users = req.body;
+    const {users} = req.body;
 
     if (!Array.isArray(users) || users.length === 0) {
       return res.status(400).json({ error: "Invalid input. Provide an array of users." });
